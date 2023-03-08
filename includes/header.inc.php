bb1a7e6834a027434ec97b4d5b3e4d3f59553329
Dn1San <c0009613@hallam.shu.ac.uk>
2023-03-08 16:00:43 +0000

    Main Header css

--- includes/header.inc.php
+++ includes/header.inc.php
@@ -5,9 +5,9 @@
                 <li><a href="create.php">Create Tutor/Student</a></li>
                 <li><a href="permissions.php">Permissions</a></li>
                 <li><a href="progress.php">Progress</a></li>
+                <div id="sign-out">
+                    <li><a href="login.php">Sign Out</a></li>
+                </div>
             </ul>
-            <div id="sign-out">
-                <a href="login.php">Sign Out</a>
-            </div>
         </nav>
 </header>
